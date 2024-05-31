50089631e336313046837e6ca2df47117fbcffcc
Ahmed Aboeita <108438941+AhmedAbo-Eita@users.noreply.github.com>
2024-06-01 00:26:08 +0300

    Update main.c

--- Struct/Ex2/main.c
+++ Struct/Ex2/main.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-void grade(int x );
+void grade(float x );
 
 int num,i;
 
@@ -25,7 +25,7 @@ int main()
     {
         printf("Enter the name of student number %d = ", i+1);
         fflush(stdin);fflush(stdout);
-        scanf("%d", &students[i].name);
+        scanf("%s", &students[i].name);
 
         printf("Enter the age of student number %d = ", i+1);
         fflush(stdin);fflush(stdout);
@@ -33,20 +33,26 @@ int main()
 
         printf("Enter the GPA of student number %d = ", i+1);
         fflush(stdin);fflush(stdout);
-        scanf("%d", &students[i].GPA);
+        scanf("%f", &students[i].GPA);
+    }
+    
+    for(i=0 ; i<num ; i++)
+    {
+        printf("Name: %s Age: %d GPA: %0.2f ", students[i].name, students[i].age, students[i].GPA);
+        grade(students[i].GPA);
     }
 
     return 0;
 }
 
-void grade(int x )
+void grade(float x )
 {
-    if (x>=3.3)
-    printf("Grade: Excelent");
-    else if (3.3>x>=2.8)
-    printf("Grade: Very Good");
-    else if (2.8>x>2)
-    printf("Grade: Good");
-    else
-    printf("Grade: Fail");
+    if (x >= 3.3)
+    {printf("Grade: Excelent\n");}
+    else if (2.8 <= x && x <3.3)
+    {printf("Grade: Very Good\n");}
+    else if (2 <= x && x <2.8)
+    {printf("Grade: Good\n");}
+    else if (x <2.0)
+    printf("Grade: Fail\n");
 }
